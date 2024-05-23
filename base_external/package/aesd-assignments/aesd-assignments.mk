936cce7882a9f60641bbb0d6afdddede089bb00a
Dharani Vel P <106021899+dharanivelp@users.noreply.github.com>
2024-05-23 13:24:51 +0530

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'c546345a88efccd6ad92eadf10e5b343299a1bcd'
+AESD_ASSIGNMENTS_VERSION = '038521d485935cac21ea1e67d3e4f10d0c6dd260'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
